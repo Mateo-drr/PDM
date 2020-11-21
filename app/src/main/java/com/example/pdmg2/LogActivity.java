4e20a6693f675dc6d7feea8b849c5ad0cb473fcb
Mateo-drr <mateodrr@gmail.com>
2020-11-21 17:56:47 +0000

    11/21 main activity launches

--- app/src/main/java/com/example/pdmg2/LogActivity.java
+++ app/src/main/java/com/example/pdmg2/LogActivity.java
@@ -1,5 +1,6 @@
 package com.example.pdmg2;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +26,7 @@ public class LogActivity extends AppCompatActivity {
     private EditText regpass;
     private EditText regpass2;
     private EditText regname;
+
     private String userregname;
     private String userregemail;
     private String userregpass;
@@ -40,6 +42,7 @@ public class LogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ViewPager viewPager = findViewById(R.id.viewPager);
+
         LogActivity.AuthenticationPagerAdapter pagerAdapter = new LogActivity.AuthenticationPagerAdapter(getSupportFragmentManager());
         pagerAdapter.addFragmet(new LoginFragment());
         pagerAdapter.addFragmet(new RegisterFragment());
@@ -47,7 +50,7 @@ public class LogActivity extends AppCompatActivity {
 
     }
 
-    public void onClickRegister(View view) {
+    public void OnClickRegister(View view) {
         //read register data
         regname = findViewById(R.id.txtregname);
         regemail = findViewById(R.id.txtregemail);
@@ -60,7 +63,7 @@ public class LogActivity extends AppCompatActivity {
         userregpass2 = regpass2.getText().toString();
 
         if(userregpass.isEmpty() || userregpass2.isEmpty() || userregpass2.equals("-") || userregpass.equals("-")
-            || userregname.isEmpty() || userregname.equals("-") || userregemail.isEmpty() || userregemail.equals("-")) {
+                || userregname.isEmpty() || userregname.equals("-") || userregemail.isEmpty() || userregemail.equals("-")) {
             //AlertDialog ad = new AlertDialog.Builder(this).create();
             //ad.setMessage(getString(R.string.tst_fillEverything));
             //ad.setTitle(getString(R.string.tst_error));
@@ -71,7 +74,7 @@ public class LogActivity extends AppCompatActivity {
             Toast.makeText(this, getString(R.string.tst_fillEverything), Toast.LENGTH_SHORT).show();
         }else {
             if(userregpass.equals(userregpass2)) {
-                User ureg = new User(userregemail, userName, userPass);
+                User ureg = new User(userregemail, userregname, userregpass);
                 arr_users.add(ureg);
                 // TODO compare if email already exists
                 Toast.makeText(this, getString(R.string.tst_usercreated), Toast.LENGTH_SHORT).show();
@@ -82,12 +85,12 @@ public class LogActivity extends AppCompatActivity {
     }
 
     public void onClickLogin(View view) {
-        //read login data
+
         logemail = findViewById(R.id.txtlogemail);
         logpass = findViewById(R.id.txtlogpass);
-
+        //read login data
         userlogemail = logemail.getText().toString();
-        userPass = logpass.getText().toString();
+        userlogpass = logpass.getText().toString();
 
         if(userlogemail.isEmpty() || userlogpass.isEmpty() || userlogemail.equals("-") || userlogpass.equals("-")) {
             //AlertDialog ad = new AlertDialog.Builder(this).create();
@@ -96,16 +99,16 @@ public class LogActivity extends AppCompatActivity {
             //ad.setIcon(R.drawable.actionbar_exc);
             //ad.setButton(Dialog.BUTTON_NEUTRAL, "OK", null,null);
             //ad.show();
-
             Toast.makeText(this, getString(R.string.tst_fillEverything), Toast.LENGTH_SHORT).show();
         }else {
             //TODO
-            User ulog = new User(userlogemail, "", userPass);
+            User ulog = new User(userlogemail, "", userlogpass);
             //ulog.getEmail().compareTo();
 
             //Launch main activity
+            Toast.makeText(this, getString(R.string.tst_fillEverything), Toast.LENGTH_SHORT).show();
             Intent i = new Intent (this, MainActivity.class);
-            startActivityForResult(i, 1);
+            startActivity(i);
         }
 
     }
@@ -125,8 +128,6 @@ public class LogActivity extends AppCompatActivity {
     public void setUserPass(String userPass) {
         userPass = userPass;
     }
-
-
 
     class AuthenticationPagerAdapter extends FragmentPagerAdapter {
         private final ArrayList<Fragment> fragmentList = new ArrayList<>();
